5b854b2852da0afaea8bf05274e8378e2893bb53
EduardoLeyva <124404041+EduardoLeyva@users.noreply.github.com>
2023-02-22 00:31:58 -0600

    Tarea #4. Búsquedas No Informadas  8 Puzzle

---
+++ Unidad 1/8Puzzle/ArbolBusqueda.java
@@ -0,0 +1,73 @@
+
+package Puzzle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ArbolBusqueda {
+    
+    Nodo raiz;
+    String objetivo;
+    
+    public ArbolBusqueda(Nodo raiz, String objetivo)
+    {
+        this.raiz = raiz;
+        this.objetivo = objetivo;
+    }
+    
+    //Busqueda por Anchura
+    public void busquedaPorAnchura()
+    {
+        Nodo nodoActual = raiz;
+        Collection<String> estadosVisitados = new ArrayList();
+        Queue<Nodo> estadosPorVisitar = new LinkedList();
+        while(!nodoActual.getEstado().equals(objetivo))
+        {
+            estadosVisitados.add(nodoActual.getEstado());
+            //Generar a los Nodos Hijos
+            Collection<String> hijos = nodoActual.generaHijos();	//<-- Cada Equipo tiene que ingeniarselas para crear este metodo!
+            for (String hijo : hijos) {
+                if(!estadosVisitados.contains(hijo))
+                {
+                    //System.out.println("---Metiendo nuevo Nodo");
+                    //Crear nuevo Nodo.
+                    Nodo nHijo = new Nodo(hijo);
+                    nHijo.setPadre(nodoActual);
+                    estadosPorVisitar.add(nHijo);
+                }
+            }
+            nodoActual = estadosPorVisitar.poll();
+        }
+        System.out.println(nodoActual.imprimirSolucion());
+    }
+    
+    //Busqueda por Profunfidad
+    public void busquedaPorProfundidad()
+    {
+        Nodo nodoActual = raiz;
+        Collection<String> estadosVisitados = new ArrayList();
+        Stack<Nodo> estadosPorVisitar = new Stack();
+        while(!nodoActual.getEstado().equals(objetivo))
+        {
+            estadosVisitados.add(nodoActual.getEstado());
+            //Generar a los Nodos Hijos
+            Collection<String> hijos = nodoActual.generaHijos();	//<-- Cada Equipo tiene que ingeniarselas para crear este metodo!
+            for (String hijo : hijos) {
+                if(!estadosVisitados.contains(hijo))
+                {
+                    //System.out.println("---Metiendo nuevo Nodo");
+                    //Crear nuevo Nodo.
+                    Nodo nHijo = new Nodo(hijo);
+                    nHijo.setPadre(nodoActual);
+                    estadosPorVisitar.add(nHijo);
+                }
+            }
+            nodoActual = estadosPorVisitar.pop();
+        }
+        System.out.println(nodoActual.imprimirSolucion());
+    }
+    
+}
---
+++ Unidad 1/8Puzzle/Nodo.java
@@ -0,0 +1,219 @@
+
+package Puzzle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Nodo {
+    private String estado;
+    private Collection<Nodo> hijos;
+    Nodo padre;
+    
+    public Nodo(String estado)
+    {
+        this.estado = estado;
+        hijos = new ArrayList<>();
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Collection<Nodo> getHijos() {
+        return hijos;
+    }
+
+    //Agrega 1 Nodo hijo
+    public void agregarHijos(Nodo h) {
+        this.hijos.add(h);
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public Collection<String> generaHijos() {
+        //|0|1|2|
+        //|3|4|5|   Indices
+        //|6|7|8|
+    	ArrayList<String>hijosGenerados=new ArrayList<>();
+        int i = estado.indexOf(" ");//Obtengo el indice del espacio
+        String estadoTemp;
+        switch(i)
+        {
+        /*
+		Usar los métodos: replace, charAt
+		Otra idea es pasar el String a Matriz, hacer los cambios y luego pasarlo a String... xD		
+		*/
+            case 0:
+      
+            	estadoTemp=""+estado.charAt(1)+estado.charAt(0)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(3)+estado.charAt(1)+estado.charAt(2)+estado.charAt(0)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+
+               
+                break;
+            case 1:
+            	estadoTemp=""+estado.charAt(1)+estado.charAt(0)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(2)+estado.charAt(1)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(4)+estado.charAt(2)+estado.charAt(3)+estado.charAt(1)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            
+                break;
+            case 2:
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(2)+estado.charAt(1)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(5)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(2)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	
+            	break;
+            case 3:
+            	estadoTemp=""+estado.charAt(3)+estado.charAt(1)+estado.charAt(2)+estado.charAt(0)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(4)+estado.charAt(3)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(6)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(3)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	break;
+            case 4: 
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(4)+estado.charAt(2)+estado.charAt(3)+estado.charAt(1)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(4)+estado.charAt(3)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(5)
+            	+estado.charAt(4)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(7)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(4)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+
+            	break;
+            case 5:
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(5)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(2)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(5)
+            	+estado.charAt(4)+estado.charAt(6)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(8)+estado.charAt(6)+estado.charAt(7)+estado.charAt(5);
+            	hijosGenerados.add(estadoTemp);
+
+            	break;
+            case 6:
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(6)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(3)+estado.charAt(7)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(7)+estado.charAt(6)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	break;
+            case 7:
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(7)+estado.charAt(6)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(7)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(4)+estado.charAt(8);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(8)+estado.charAt(7);
+            	hijosGenerados.add(estadoTemp);
+            	break;
+            case 8:
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(5)+estado.charAt(6)+estado.charAt(8)+estado.charAt(7);
+            	hijosGenerados.add(estadoTemp);
+            	
+            	estadoTemp=""+estado.charAt(0)+estado.charAt(1)+estado.charAt(2)+estado.charAt(3)+estado.charAt(4)
+            	+estado.charAt(8)+estado.charAt(6)+estado.charAt(7)+estado.charAt(5);
+            	hijosGenerados.add(estadoTemp);
+            	break;
+       }
+        
+        
+        return hijosGenerados;
+    }
+    
+    
+    public String imprimirSolucion() {
+
+        int estado = 0;
+        Nodo n = this;
+        Nodo aux = this;
+
+        int nNodos = 1;
+        while(n != null){
+                 nNodos++;
+              n = n.getPadre();
+        }
+        n = aux;
+          String salida = "";
+          while(n != null){
+                  estado++;
+              salida = n.getEstadoImprimir(nNodos  - estado)+"\n"+salida;
+              n = n.getPadre();
+        }
+         return salida;
+
+    }
+
+     public String getEstadoImprimir(int estado) {
+
+         String filaGuion = "\t+---+---+---+\n";
+
+         String flecha = "\t      _ \r\n"
+                       + "\t     | |\r\n"
+                       + "\t    _| |_\r\n"
+                       + "\t    \\   /\r\n"
+                       + "\t     \\ /\r\n"
+                       + "\t      V\n\n";
+
+         return  flecha+"\n"
+                +"\tEstado # "+estado+"\n\n"
+                +filaGuion
+                +"\t| "+this.estado.charAt(0)+" | "+this.estado.charAt(1)+" | "+this.estado.charAt(2)+" |\n"
+                +filaGuion
+                +"\t| "+this.estado.charAt(3)+" | "+this.estado.charAt(4)+" | "+this.estado.charAt(5)+" |\n"
+                +filaGuion
+                +"\t| "+this.estado.charAt(6)+" | "+this.estado.charAt(7)+" | "+this.estado.charAt(8)+" |\n"
+                +filaGuion;
+     }
+}
